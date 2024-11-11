060ef7154b74250b6e44914101b3f0cfe37007cc
Aldrin Morris D. Balitbit <aldrinbalitbit1127@gmail.com>
2024-11-11 14:06:09 +0800

    CIs added

--- build.sh
+++ build.sh
@@ -1,14 +1,32 @@
 #!/bin/bash -e
 _work_dir="$(pwd)"
+_dl_dir="${_work_dir}/.dl"
 _sources_dir="${_work_dir}/sources"
 
+! [ -d "${_dl_dir}"      ] && mkdir -p "${_dl_dir}"
 ! [ -d "${_sources_dir}" ] && mkdir -p "${_sources_dir}"
+
+rm -rf "${_sources_dir}/dl"
+dl_data="$(cat "${_work_dir}"/dls.json)"
+printf "${dl_data}" | jq -c '.[] | {name: .name, url: .url, filename: .filename}' | while read -r line; do
+    name="$(    printf "${line}" | jq -r      '.name')"
+    url="$(     printf "${line}" | jq -r       '.url')"
+    filename="$(printf "${line}" | jq -r  '.filename')"
+    if ! [ -f "${_dl_dir}/${filename}" ]; then
+      echo "Downloading ${name}..."
+      wget -q -O "${_dl_dir}/${filename}" "${url}"
+    fi
+    ! [ -d "${_sources_dir}/dl/${name}" ] && mkdir -p "${_sources_dir}/dl/${name}"
+    echo "Extracting ${name}..."
+    tar -xf "${_dl_dir}/${filename}" -C "${_sources_dir}/dl/${name}" --strip-components=1
+done
 
 clone_data="$(cat "${_work_dir}"/clones.json)"
 printf "${clone_data}" | jq -c '.[] | {name: .name, url: .url}' | while read -r line; do
     name="$(printf "${line}" | jq -r '.name')"
-    url="$( printf "${line}" | jq -r '.url' )"
-    echo "${name}: ${url}"
+    url="$( printf "${line}" | jq -r  '.url')"
+    echo "Cloning ${name}..."
+    git clone --depth=1 "${url}" "${_sources_dir}/clone/${name}" > /dev/null 2>&1
 done
 
 # _platform="${1}"
